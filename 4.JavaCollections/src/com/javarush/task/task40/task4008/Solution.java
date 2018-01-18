0f7741e265aa516de35fe7438ee23f444a509bb5
Artur <avagyanart@yandex.ru>
2018-01-18 23:56:44 +0300

    javarush

--- 4.JavaCollections/src/com/javarush/task/task40/task4008/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4008/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.task40.task4008;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 
 /* 
 Работа с Java 8 DateTime API
@@ -10,7 +13,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Solution {
     public static void main(String[] args) {
-        printDate("21.4.2014 15:56:45");
+        printDate("9.10.2017 5:56:45");
         System.out.println();
         printDate("21.4.2014");
         System.out.println();
@@ -18,6 +21,35 @@ public class Solution {
     }
 
     public static void printDate(String date) {
-        //напишите тут ваш код
+        String[] strings = date.split(" ");
+        if (strings[0].contains(".")) {
+            printDateFirst(strings[0], DateTimeFormatter.ofPattern("d.M.y"));
+            if (strings.length > 1 && strings[1].contains(":"))
+                printDateSecond(strings[1], DateTimeFormatter.ofPattern("H:m:s"));
+        }
+        else if (strings[0].contains(":")){
+            printDateSecond(strings[0], DateTimeFormatter.ofPattern("H:m:s"));
+        }
+    }
+
+    private static void printDateFirst(String string, DateTimeFormatter formatter) {
+        LocalDate date = LocalDate.parse(string, formatter);
+        System.out.println("День: " + date.getDayOfMonth());
+        System.out.println("День недели: " + date.getDayOfWeek().getValue());
+        System.out.println("День месяца: " + date.getDayOfMonth());
+        System.out.println("День года: " + date.getDayOfYear());
+        System.out.println("Неделя месяца: " + date.get(WeekFields.of(Locale.getDefault()).weekOfMonth()));
+        System.out.println("Неделя года: " + (date.get(WeekFields.of(Locale.getDefault()).weekOfYear())));
+        System.out.println("Месяц: " + date.getMonthValue());
+        System.out.println("Год: " + date.getYear());
+    }
+
+    private static void printDateSecond(String string, DateTimeFormatter formatter) {
+        LocalTime localTime = LocalTime.parse(string, formatter);
+        System.out.println("AM или PM: " + (localTime.get(ChronoField.AMPM_OF_DAY)==0?"AM":"PM"));
+        System.out.println("Часы: " + localTime.get(ChronoField.HOUR_OF_AMPM));
+        System.out.println("Часы дня: " + localTime.getHour());
+        System.out.println("Минуты: " + localTime.getMinute());
+        System.out.println("Секунды: " + localTime.getSecond());
     }
 }
