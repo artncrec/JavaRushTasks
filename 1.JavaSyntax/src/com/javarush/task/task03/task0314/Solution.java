de1e2e9b9e0aa61365c6e62b37db94eff067ddda
Artur <avagyanart@yandex.ru>
2018-02-02 22:59:14 +0300

    javarush

--- 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -6,6 +6,11 @@ package com.javarush.task.task03.task0314;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
     }
 }
