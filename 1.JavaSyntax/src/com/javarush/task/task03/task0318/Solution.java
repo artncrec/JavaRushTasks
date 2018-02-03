1c82143b04448ce40a5f10bad56712dcd29ec4ff
Artur <avagyanart@yandex.ru>
2018-02-04 01:00:36 +0300

    javarush

--- 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -8,6 +8,9 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int age = Integer.parseInt(reader.readLine());
+        String name = reader.readLine();
+        System.out.println(name + " захватит мир через " + age + " лет. Му-ха-ха!");
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -8,6 +8,10 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int i = Integer.parseInt(reader.readLine());
+        int j = Integer.parseInt(reader.readLine());
+        System.out.println(name + " получает " + i + " через " + j + " лет.");
     }
 }
