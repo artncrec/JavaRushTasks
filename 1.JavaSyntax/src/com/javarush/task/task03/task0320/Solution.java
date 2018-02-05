a23b052363e4e2ff4dc11153944299f25058ba9a
Artur <avagyanart@yandex.ru>
2018-02-05 23:43:56 +0300

    javarush

--- 1.JavaSyntax/src/com/javarush/task/task03/task0320/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0320/Solution.java
@@ -9,6 +9,8 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        System.out.println(name + " зарабатывает $5,000. Ха-ха-ха!");
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -8,6 +8,8 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        System.out.println("Я буду зарабатывать $" + n + " в час");
     }
 }
