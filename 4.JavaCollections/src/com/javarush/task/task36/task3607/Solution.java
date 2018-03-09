f468ad931ba14144b1c53182105f2b7e6197c2fe
Artur <avagyanart@yandex.ru>
2018-03-09 22:27:15 +0300

    javarush

--- 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task36.task3607;
 
+import java.lang.reflect.Constructor;
 import java.util.Collection;
 import java.util.*;
 
@@ -12,11 +13,27 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
-        String listClass = Solution.class.getProtectionDomain().getCodeSource().getLocation().toString();
-//        for (Class c:listClass)
-//            System.out.println(c);
-        
-        System.out.println(listClass);
+        System.out.println(Solution.class.getProtectionDomain().getCodeSource().getLocation());
+        Class[] listClasses = Queue.class.getDeclaredClasses();
+        for (Class<?> clazz : listClasses) {
+            Class superClass = clazz;
+            List<Class> superClassArray = new ArrayList<>();
+            while ((superClass = superClass.getSuperclass()) != null)
+                superClassArray.add(superClass);
+            for (Class supers : superClassArray)
+                for (Class interf : supers.getInterfaces())
+                    if (interf.getSimpleName().equals("Queue")) {
+                        try {
+                            Constructor constructor = clazz.getDeclaredConstructor();
+                            constructor.setAccessible(true);
+                            List result = (List) constructor.newInstance();
+                            result.get(0);
+                        } catch (IndexOutOfBoundsException e) {
+                            return clazz;
+                        } catch (Exception e) {
+                        }
+                    }
+        }
         return null;
     }
 }
