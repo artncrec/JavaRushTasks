245f3fb8101ab0933d71b7296b6d94387860426c
Artur <avagyanart@yandex.ru>
2019-01-03 00:41:46 +0300

    javarush

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -9,20 +9,81 @@ public class Snake {
     private List<GameObject> snakeParts = new ArrayList<>();
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
     private static final String BODY_SIGN = "\u26AB";
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
 
     public Snake(int x, int y) {
-        GameObject first = new GameObject(x,y);
-        GameObject second = new GameObject(x+1,y);
-        GameObject third = new GameObject(x+2,y);
+        GameObject first = new GameObject(x, y);
+        GameObject second = new GameObject(x + 1, y);
+        GameObject third = new GameObject(x + 2, y);
         snakeParts.add(first);
         snakeParts.add(second);
         snakeParts.add(third);
     }
 
     public void draw(Game game) {
-        game.setCellValue(snakeParts.get(0).x, snakeParts.get(0).y, HEAD_SIGN);
+        if (isAlive)
+            game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SIGN, Color.GREEN, 75);
+        else
+            game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SIGN, Color.RED, 75);
         for (int i = 1; i < snakeParts.size(); i++) {
-            game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, BODY_SIGN);
+            if (isAlive)
+                game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SIGN, Color.GREEN, 75);
+            else
+                game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SIGN, Color.RED, 75);
         }
+    }
+
+    public void setDirection(Direction direction) {
+        if (direction.equals(Direction.LEFT) && this.direction.equals(Direction.RIGHT) ||
+              direction.equals(Direction.RIGHT) && this.direction.equals(Direction.LEFT) ||
+              direction.equals(Direction.UP) && this.direction.equals(Direction.DOWN) ||
+              direction.equals(Direction.DOWN) && this.direction.equals(Direction.UP))
+            ;
+        else
+            this.direction = direction;
+    }
+
+    public void move(Apple apple) {
+        GameObject head = createNewHead();
+        if (head.x < 0 || head.y < 0 || head.x > SnakeGame.WIDTH - 1 || head.y > SnakeGame.HEIGHT - 1)
+            isAlive = false;
+        else {
+            isAlive = !checkCollision(head);
+            if (isAlive) {
+                snakeParts.add(0, head);
+                if (head.x == apple.x && head.y == apple.y) {
+                    apple.isAlive = false;
+                }
+                else {
+                    removeTail();
+                }
+            }
+        }
+    }
+
+    public GameObject createNewHead() {
+        int x = snakeParts.get(0).x;
+        int y = snakeParts.get(0).y;
+        if (direction.equals(Direction.LEFT))
+            x--;
+        else if (direction.equals(Direction.RIGHT))
+            x++;
+        else if (direction.equals(Direction.UP))
+            y--;
+        else if (direction.equals(Direction.DOWN))
+            y++;
+        return new GameObject(x, y);
+    }
+
+    public void removeTail() {
+        snakeParts.remove(snakeParts.size() - 1);
+    }
+
+    public boolean checkCollision(GameObject gameObject) {
+        for (GameObject object : snakeParts)
+            if (object.x == gameObject.x && object.y == gameObject.y)
+                return true;
+        return false;
     }
 }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -5,8 +5,10 @@ import com.javarush.engine.cell.*;
 public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private int turnDelay;
 
     private Snake snake;
+    private Apple apple;
 
     @Override
     public void initialize() {
@@ -14,17 +16,47 @@ public class SnakeGame extends Game {
         createGame();
     }
 
-    private void createGame(){
-        snake = new Snake(WIDTH/2, HEIGHT/2);
+    private void createGame() {
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        createNewApple();
         drawScene();
     }
 
     private void drawScene(){
         for (int i = 0; i < getScreenWidth(); i++) {
             for (int j = 0; j < getScreenHeight(); j++) {
-                setCellColor(i,j,Color.BURLYWOOD);
+                setCellValueEx(i, j, Color.BEIGE, "");
             }
         }
         snake.draw(this);
+        apple.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move(apple);
+        if (!apple.isAlive)
+            createNewApple();
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (key.equals(Key.LEFT))
+            snake.setDirection(Direction.LEFT);
+        else if (key.equals(Key.UP))
+            snake.setDirection(Direction.UP);
+        else if (key.equals(Key.RIGHT))
+            snake.setDirection(Direction.RIGHT);
+        else if (key.equals(Key.DOWN))
+            snake.setDirection(Direction.DOWN);
+    }
+
+    private void createNewApple(){
+        int w = getRandomNumber(14);
+        int h = getRandomNumber(14);
+        apple = new Apple(w,h);
     }
 }
