beb2dfd501462cc03f3ecca10ae39632066b6681
Artur <avagyanart@yandex.ru>
2019-01-08 23:05:03 +0300

    javarush

--- 8.Games/src/com/javarush/games/racer/road/RoadManager.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadManager.java
@@ -1,12 +1,27 @@
 package com.javarush.games.racer.road;
 
+import com.javarush.engine.cell.Game;
 import com.javarush.games.racer.RacerGame;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoadManager {
     public static final int LEFT_BORDER = RacerGame.ROADSIDE_WIDTH, RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
-    private RoadObject createRoadObject(RoadObjectType type, int x, int y){
-        if (type==RoadObjectType.THORN)
-            return new Thorn(x,y);
+    private static final int FIRST_LANE_POSITION = 16, FOURTH_LANE_POSITION = 44;
+    private List<RoadObject> items = new ArrayList<>();
+
+    private void addRoadObject(RoadObjectType type, Game game) {
+        int x = game.getRandomNumber(FIRST_LANE_POSITION, FOURTH_LANE_POSITION);
+        int y = -1 * RoadObject.getHeight(type);
+        RoadObject object = createRoadObject(type, x, y);
+        if (object != null)
+            items.add(object);
+    }
+
+    private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
+        if (type == RoadObjectType.THORN)
+            return new Thorn(x, y);
         else return null;
     }
 }
